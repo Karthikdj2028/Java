c5ddcd64eb2d607c7a4e443d648436fc3a7aeeb4
Karthikdj2028 <112675192+Karthikdj2028@users.noreply.github.com>
2024-07-21 16:28:10 +0530

    Add files via upload

---
+++ Day 7/upLeftTri.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class upLeftTri {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        sc.close();
+        System.out.println("Using '*': ");
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nUsing i: ");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        System.out.println("\nUsing j: ");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
